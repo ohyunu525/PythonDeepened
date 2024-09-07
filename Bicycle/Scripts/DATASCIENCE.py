e292f3063907151e5db97fe1c53fdc90e35d9864
ohyunu525 <5252xkdgnfn@gmail.com>
2024-09-07 16:26:17 +0900

    2024-09-07
    RandomForest

--- Bicycle/Scripts/DATASCIENCE.py
+++ Bicycle/Scripts/DATASCIENCE.py
@@ -12,8 +12,35 @@ train = pd.read_csv("../Sources/train.csv", parse_dates=["datetime"])
 
 test = pd.read_csv("../Sources/test.csv", parse_dates=["datetime"])
 
-print(train.shape)
+train["year"] = train["datetime"].dt.year
+train["month"] = train["datetime"].dt.month
+train["day"] = train["datetime"].dt.day
+train["hour"] = train["datetime"].dt.hour
+train["minute"] = train["datetime"].dt.minute
+train["second"] = train["datetime"].dt.second
+train["dayofweek"] = train["datetime"].dt.dayofweek
 
-print(test.shape)
+test["year"] = test["datetime"].dt.year
+test["month"] = test["datetime"].dt.month
+test["day"] = test["datetime"].dt.day
+test["hour"] = test["datetime"].dt.hour
+test["minute"] = test["datetime"].dt.minute
+test["second"] = test["datetime"].dt.second
+test["dayofweek"] = test["datetime"].dt.dayofweek
+
+fig, axes = plt.subplots(nrows=2)
+fig.set_size_inches(18,15)
+
+plt.sca(axes[0])
+plt.xticks(rotation=30, ha='right')
+axes[0].set(ylabel='Count', title="train windspeed")
+sns.countplot(data=train, x="windspeed", ax=axes[0])
+
+plt.sca(axes[1])
+plt.xticks(rotation=30, ha='right')
+axes[1].set(ylabel='Count', title="test windspeed")
+sns.countplot(data=test, x="windspeed", ax=axes[1])
+
+plt.savefig("TrainTestWindSpeedBy")
 
 #\[T]/
