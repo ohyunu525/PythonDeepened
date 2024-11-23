c4f97b728ad5eee705c977196eb075a766dd1671
ohyunu525 <5252xkdgnfn@gmail.com>
2024-11-23 17:00:02 +0900

    2024-11-23

--- Health-check/Scripts/Protoss.py
+++ Health-check/Scripts/Protoss.py
@@ -30,7 +30,7 @@ df = pd.read_csv("../Sources/NHIS_OPEN_GJ_2017_v1.1.csv", encoding="CP949")
 
 createDirectory("../Outputs/CategoricalData")
 
-df_sample = df.sample(1000, random_state = 1)
+df_sample = df.sample(1000, random_state=1)
 
 df["음주여부"].value_counts().plot.bar()
 plt.savefig("../Outputs/CategoricalData/CategoricalData_1")
@@ -52,20 +52,47 @@ sns.countplot(data=df, x="연령대코드(5세단위)", hue="음주여부")
 plt.savefig("../Outputs/CategoricalData/CategoricalData_6")
 
 plt.figure(figsize=(15, 4))
-sns.countplot(data=df, x="신장(5cm단위)")
+sns.countplot(data=df, x="신장(5Cm단위)")
 plt.savefig("../Outputs/CategoricalData/CategoricalData_7")
 
 plt.figure(figsize=(15, 4))
-sns.countplot(data=df, x="체중(5kg단위)")
+sns.countplot(data=df, x="체중(5Kg단위)")
 plt.savefig("../Outputs/CategoricalData/CategoricalData_8")
 
 plt.figure(figsize=(15, 4))
-sns.countplot(data=df, x="신장(5cm단위)", hue="성별코드")
+sns.countplot(data=df, x="신장(5Cm단위)", hue="성별코드")
 plt.savefig("../Outputs/CategoricalData/CategoricalData_9")
 
 plt.figure(figsize=(15, 4))
-sns.countplot(data=df, x="체중(5kg단위)", hue="성별코드")
+sns.countplot(data=df, x="체중(5Kg단위)", hue="성별코드")
 plt.savefig("../Outputs/CategoricalData/CategoricalData_10")
 
-sns.barplot(data=df, x="연령대코드(5세단위)")
+sns.barplot(data=df, x="연령대코드(5세단위)", y="총콜레스테롤")
+plt.savefig("../Outputs/CategoricalData/CategoricalData_11")
+
+sns.barplot(data=df_sample, x="연령대코드(5세단위)", y="총콜레스테롤")
+plt.savefig("../Outputs/CategoricalData/CategoricalData_12")
+
+sns.barplot(data=df_sample, x="연령대코드(5세단위)", y="총콜레스테롤", hue="음주여부")
+plt.savefig("../Outputs/CategoricalData/CategoricalData_13")
+
+plt.figure(figsize=(12, 4))
+sns.barplot(data=df_sample, x="연령대코드(5세단위)", y="총콜레스테롤", hue="흡연상태")
+plt.savefig("../Outputs/CategoricalData/CategoricalData_14")
+
+plt.figure(figsize=(12, 4))
+sns.barplot(data=df_sample, x="연령대코드(5세단위)", y="트리글리세라이드", hue="음주여부")
+plt.savefig("../Outputs/CategoricalData/CategoricalData_15")
+
+plt.figure(figsize=(12, 4))
+sns.barplot(data=df, x="연령대코드(5세단위)", y="트리글리세라이드", hue="음주여부", ci=95)
+plt.savefig("../Outputs/CategoricalData/CategoricalData_16")
+
+plt.figure(figsize=(12, 4))
+sns.barplot(data=df, x="연령대코드(5세단위)", y="트리글리세라이드", hue="음주여부", ci="sd")
+plt.savefig("../Outputs/CategoricalData/CategoricalData_17")
+
+plt.figure(figsize=(12, 4))
+sns.barplot(data=df, x="연령대코드(5세단위)", y="체중(5Kg단위)", hue="성별코드", ci=None)
+plt.savefig("../Outputs/CategoricalData/CategoricalData_18")
 #\[T]/
